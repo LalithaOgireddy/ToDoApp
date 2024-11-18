41d317cecc8070bc43960dbddb828fe7cffdd75c
Lalitha Ogireddy <lalitha.ogireddy@gmail.com>
2024-11-18 08:43:23 +0100

    Implemented DAO for ToDoItem and TodoItemTask

--- src/main/java/com/lalitha/data/dao/TodoItemDAOCollection.java
+++ src/main/java/com/lalitha/data/dao/TodoItemDAOCollection.java
@@ -1,54 +1,89 @@
 package com.lalitha.data.dao;
 
+import com.lalitha.model.AppUser;
+import com.lalitha.model.Person;
 import com.lalitha.model.TodoItem;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class TodoItemDAOCollection implements TodoItemDAO{
+
+    private Collection<TodoItem> todoItems;
     @Override
     public TodoItem persist(TodoItem todoItem) {
-        return null;
+        TodoItem newTodoItem = new TodoItem(todoItem.getTitle(),todoItem.getTaskDescription(),todoItem.getDeadLine(),todoItem.getCreator());
+        todoItems.add(newTodoItem);
+        return newTodoItem;
     }
 
     @Override
     public TodoItem findById(int id) {
+        for(TodoItem item : todoItems){
+            if(item.getId() == id) return item;
+        }
         return null;
     }
 
     @Override
     public Collection<TodoItem> findAll() {
-        return List.of();
+        return Collections.unmodifiableCollection(todoItems);
     }
 
     @Override
     public Collection<TodoItem> findAllByDoneStatus(boolean done) {
-        return List.of();
+        Collection<TodoItem> doneList = new ArrayList<>();
+        for(TodoItem item : todoItems){
+            if(item.isDone()) doneList.add(item);
+        }
+        return doneList;
     }
 
     @Override
     public Collection<TodoItem> findByTitleContains(String title) {
-        return List.of();
+        Collection<TodoItem> titleList = new ArrayList<>();
+        for(TodoItem item : todoItems){
+            if(item.getTitle().trim().equals(title.trim())) titleList.add(item);
+        }
+        return titleList;
     }
 
     @Override
     public Collection<TodoItem> findByPersonId(int personId) {
-        return List.of();
+        Collection<TodoItem> personList = new ArrayList<>();
+        for(TodoItem item : todoItems){
+            if(item.getId() == personId) personList.add(item);
+        }
+        return personList;
     }
 
     @Override
     public Collection<TodoItem> findByDeadlineBefore(LocalDate date) {
-        return List.of();
+        Collection<TodoItem> dlBeforeList = new ArrayList<>();
+        for(TodoItem item : todoItems){
+            if(!item.isOverdue()) dlBeforeList.add(item);
+        }
+        return dlBeforeList;
     }
 
     @Override
     public Collection<TodoItem> findByDeadlineAfter(LocalDate date) {
-        return List.of();
+        Collection<TodoItem> dlAfterList = new ArrayList<>();
+        for(TodoItem item : todoItems){
+            if(item.isOverdue()) dlAfterList.add(item);
+        }
+        return dlAfterList;
     }
 
     @Override
     public void remove(int id) {
-
+        for(TodoItem item : todoItems){
+            if(item.getId() == id) {
+                todoItems.remove(item);
+            }
+        }
     }
 }
--- src/main/java/com/lalitha/data/dao/TodoItemTaskDAOCollection.java
+++ src/main/java/com/lalitha/data/dao/TodoItemTaskDAOCollection.java
@@ -1,38 +1,59 @@
 package com.lalitha.data.dao;
 
+import com.lalitha.model.TodoItem;
 import com.lalitha.model.TodoItemTask;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class TodoItemTaskDAOCollection implements TodoItemTaskDAO{
+    private Collection<TodoItemTask> todoItemTasks;
     @Override
     public TodoItemTask persist(TodoItemTask todoItemTask) {
-        return null;
+        TodoItemTask newTodoItemTask = new TodoItemTask(todoItemTask.getTodoItem(),todoItemTask.getAssignee());
+        todoItemTasks.add(newTodoItemTask);
+        return newTodoItemTask;
     }
 
     @Override
     public TodoItemTask findById(int id) {
+        for(TodoItemTask task : todoItemTasks){
+            if(task.getId() == id) return task;
+        }
         return null;
     }
 
     @Override
     public Collection<TodoItemTask> findAll() {
-        return List.of();
+        return Collections.unmodifiableCollection(todoItemTasks);
     }
 
     @Override
     public Collection<TodoItemTask> findByAssignedStatus(boolean status) {
-        return List.of();
+        Collection<TodoItemTask> assignedList = new ArrayList<>();
+        for(TodoItemTask task : todoItemTasks){
+            if(task.isAssigned() == status) assignedList.add(task);
+        }
+        return assignedList;
     }
 
     @Override
     public Collection<TodoItemTask> findByPersonId(int personId) {
-        return List.of();
+        Collection<TodoItemTask> personList = new ArrayList<>();
+        for(TodoItemTask task : todoItemTasks){
+            if(task.getId() == personId) personList.add(task);
+        }
+        return personList;
     }
 
     @Override
     public void remove(int id) {
-
+        for(TodoItemTask task : todoItemTasks){
+            if(task.getId() == id) {
+                todoItemTasks.remove(task);
+            }
+        }
     }
 }
